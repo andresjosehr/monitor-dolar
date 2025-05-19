1cf3f628eaa8249f81d3bde825fd613b58547a53
Andres <andresjosehr@gmail.com>
2025-05-19 18:51:45 -0400

    headless true

--- node/src/services/exchange/syklo.service.js
+++ node/src/services/exchange/syklo.service.js
@@ -14,7 +14,7 @@ class SykloService {
 
         while (retryCount < maxRetries) {
             const browser = await puppeteer.launch({
-              headless: false,
+              headless: true,
               args: ['--no-sandbox', '--disable-setuid-sandbox']
 
              });
